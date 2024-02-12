6a47803ce1bcd6d48f2be8c9b8ca8e989f0cfd37
CommandCenterBengkulu <commandcenterunib@gmail.com>
2024-02-12 10:02:23 +0700

    db

--- bstravel.sql
+++ bstravel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 29 Jan 2024 pada 09.05
+-- Waktu pembuatan: 12 Feb 2024 pada 04.01
 -- Versi server: 10.4.11-MariaDB
 -- Versi PHP: 7.4.1
 
@@ -21,6 +21,50 @@ SET time_zone = "+00:00";
 --
 -- Database: `bstravel`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admuser`
+--
+
+CREATE TABLE `admuser` (
+  `idadmus` int(10) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `us` varchar(30) NOT NULL,
+  `pw` varchar(50) NOT NULL,
+  `lvl` varchar(15) NOT NULL,
+  `aff` varchar(10) NOT NULL,
+  `hp` varchar(20) NOT NULL,
+  `status` varchar(30) NOT NULL,
+  `entri` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `admuser`
+--
+
+INSERT INTO `admuser` (`idadmus`, `nama`, `us`, `pw`, `lvl`, `aff`, `hp`, `status`, `entri`) VALUES
+(1, 'Admin', 'adminbst', '5cbbf65923f824b65961c8436d24b96d', 'admin', '', '', 'aktif', '2024-02-10 05:02:23'),
+(2, 'Yunus Yuwono', '082183925322', '6051e9bdf8241b9e6b9b240539446334', 'mitra', '13974', '082183925322', 'aktif', '2024-02-10 09:56:42'),
+(3, 'Thira Sandra Atika', '085380371574', 'e8cdfc09dbe31bf1512fb14ba1782d19', 'mitra', '04772', '085380371574', 'aktif', '2024-02-10 13:30:35');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `akun`
+--
+
+CREATE TABLE `akun` (
+  `idakun` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `nama` varchar(70) NOT NULL,
+  `lvl` varchar(30) NOT NULL,
+  `aff` varchar(10) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `entri` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -44,7 +88,12 @@ CREATE TABLE `bayar` (
 --
 
 INSERT INTO `bayar` (`idbayar`, `idjampaket`, `bayarke`, `nominal`, `bukti`, `rektu`, `status`, `entri`) VALUES
-(1, '1', 1, 3000000, 'WhatsApp Image 2023-12-23 at 13.24.06_c35f7b70.jpg', '11212', 'Terkirim', '2024-01-24 02:59:52');
+(1, '1', 1, 3000000, 'WhatsApp Image 2023-12-23 at 13.24.06_c35f7b70.jpg', '11212', 'Valid', '2024-01-31 08:40:40'),
+(2, '1', 2, 3500000, 'desktop-wallpaper-website-background-website-login-page-background.jpg', '11212', 'Valid', '2024-01-31 08:42:00'),
+(3, '1', 3, 5000000, 'bengkulupetatrp.png', '11212', 'Valid', '2024-02-09 09:59:44'),
+(4, '1', 4, 4000000, 'desktop-wallpaper-website-background-website-login-page-background.jpg', '11212', 'Valid', '2024-02-12 00:45:06'),
+(5, '1', 5, 3000000, 'maharani-removebg-preview.png', '11212', 'Valid', '2024-02-12 00:45:08'),
+(6, '1', 6, 8000000, '', 'Tunai', 'Valid', '2024-02-12 00:41:29');
 
 -- --------------------------------------------------------
 
@@ -73,6 +122,13 @@ CREATE TABLE `jamaah` (
   `ahliwaris` varchar(100) DEFAULT NULL,
   `hubwaris` varchar(50) DEFAULT NULL,
   `perlengkapan` varchar(20) DEFAULT NULL,
+  `kodeaff` varchar(10) NOT NULL,
+  `isof` varchar(50) NOT NULL,
+  `nopaspor` varchar(30) NOT NULL,
+  `doi` date NOT NULL,
+  `doe` date NOT NULL,
+  `relasi` varchar(40) NOT NULL,
+  `ortu` varchar(70) NOT NULL,
   `entri` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -80,10 +136,11 @@ CREATE TABLE `jamaah` (
 -- Dumping data untuk tabel `jamaah`
 --
 
-INSERT INTO `jamaah` (`idjamaah`, `nama`, `jk`, `hp`, `email`, `password`, `otp`, `token`, `tgldaftar`, `tgllahir`, `tmplahir`, `ktpsim`, `pekerjaan`, `alamat`, `kabkota`, `pos`, `foto`, `ahliwaris`, `hubwaris`, `perlengkapan`, `entri`) VALUES
-(17, 'asdfghijklaswaswaddfee', 'Laki-laki', '082183925322', 'thirasandra95@gmail.com', '202cb962ac59075b964b07152d234b70', 40882, '6051e9bdf8241b9e6b9b240539446334', NULL, '1994-10-07', 'Bengkulu', '1709030210940001', 'Karyawan Swasta', 'Seguring Hill No. 1158', 'KOTA BENGKULU', 38112, 'Yunus-red.jpg', 'Fahim Bayanaka Mahadhir', 'Anak', 'Laki-laki', '2024-01-25 08:51:50'),
-(18, 'iopupooi', 'Laki-laki', '0183081231823', 'thirasandra95@gmail.com', '202cb962ac59075b964b07152d234b70', 40882, '6051e9bdf8241b9e6b9b240539446334', NULL, '1994-10-07', 'Bengkulu', '1208308120380183', 'Karyawan Swasta', 'Seguring Hill No. 1158', 'KOTA BENGKULU', 38112, '17054737069287281381998475856585.jpg', 'Fahim Bayanaka Mahadhir', 'Anak', 'Laki-laki', '2024-01-17 12:24:15'),
-(19, 'Yunus Yuwono', 'Laki-laki', '+6282183925322', 'joewono02@gmail.com', '', 0, '', NULL, '1994-10-02', 'Bengkulu', '1709030210940001', 'Karyawan Swasta', 'Perumahan Seguring Hill No. 1158', 'Kota Bengkulu', 38116, NULL, 'Fahim Bayanaka Mahadhir', 'Anak', 'Laki-laki', '2024-01-17 18:38:52');
+INSERT INTO `jamaah` (`idjamaah`, `nama`, `jk`, `hp`, `email`, `password`, `otp`, `token`, `tgldaftar`, `tgllahir`, `tmplahir`, `ktpsim`, `pekerjaan`, `alamat`, `kabkota`, `pos`, `foto`, `ahliwaris`, `hubwaris`, `perlengkapan`, `kodeaff`, `isof`, `nopaspor`, `doi`, `doe`, `relasi`, `ortu`, `entri`) VALUES
+(17, 'asdfghijklaswaswaddfee', 'Laki-laki', '082183925322', 'thirasandra95@gmail.com', '202cb962ac59075b964b07152d234b70', 40882, '6051e9bdf8241b9e6b9b240539446334', NULL, '1994-10-07', 'Bengkulu', '1709030210940001', 'Karyawan Swasta', 'Seguring Hill No. 1158', 'KOTA BENGKULU', 38112, 'pas foto umi_page-0001(1)(1)jpg.jpg', 'Fahim Bayanaka Mahadhir', 'Anak', 'Laki-laki', '-', 'BENGKULU', 'A1234577', '2021-07-08', '2031-07-08', '', 'A/K', '2024-02-11 16:43:19'),
+(18, 'iopupooi', 'Laki-laki', '0183081231823', 'thirasandra95@gmail.com', '202cb962ac59075b964b07152d234b70', 40882, '6051e9bdf8241b9e6b9b240539446334', NULL, '1994-10-07', 'Bengkulu', '1208308120380183', 'Karyawan Swasta', 'Seguring Hill No. 1158', 'KOTA BENGKULU', 38112, '17054737069287281381998475856585.jpg', 'Fahim Bayanaka Mahadhir', 'Anak', 'Laki-laki', '', '', '', '0000-00-00', '0000-00-00', '', '', '2024-01-17 12:24:15'),
+(19, 'Yunus Yuwono', 'Laki-laki', '+6282183925322', 'joewono02@gmail.com', '', 0, '', NULL, '1994-10-02', 'Bengkulu', '1709030210940001', 'Karyawan Swasta', 'Perumahan Seguring Hill No. 1158', 'Kota Bengkulu', 38116, NULL, 'Fahim Bayanaka Mahadhir', 'Anak', 'Laki-laki', '', '', '', '0000-00-00', '0000-00-00', '', '', '2024-01-17 18:38:52'),
+(20, 'Yunus Yuwono', 'Laki-laki', '085279955778', 'joewono03@gmail.com', '', 0, '', NULL, '1994-10-02', 'Bengkulu', '1709030210940002', 'Karyawan Swasta', 'Perumahan Seguring Hill No. 1158', 'KOTA BENGKULU', 38116, 'Yunus-red.jpg', 'Fahim Bayanaka Mahadhir', 'Anak', 'Laki-laki', '13974', '', '', '0000-00-00', '0000-00-00', '', 'Yulwarno/Salbiyah', '2024-02-11 16:28:56');
 
 -- --------------------------------------------------------
 
@@ -121,6 +178,7 @@ CREATE TABLE `paket` (
   `hari` int(11) NOT NULL,
   `biaya` varchar(12) NOT NULL,
   `desk` text NOT NULL,
+  `kuota` int(11) NOT NULL,
   `entri` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -128,9 +186,10 @@ CREATE TABLE `paket` (
 -- Dumping data untuk tabel `paket`
 --
 
-INSERT INTO `paket` (`idpaket`, `nama`, `program`, `brgkt`, `hari`, `biaya`, `desk`, `entri`) VALUES
-(1, 'Umroh Bersama BST Bengkulu 10 Hari', 'Umroh', '2024-03', 10, '45000000', 'Umroh Bersama BST Bengkulu 10 Hari. Hotel bintang 5', '2024-01-18 15:36:02'),
-(2, 'Umroh Bersama BST Bengkulu 10 Hari + Turki 3 Hari', 'Umroh', '2024-01', 13, '62000000', 'Umroh Bersama BST Bengkulu 10 Hari + Turki 3 Hari', '2024-01-23 09:44:32');
+INSERT INTO `paket` (`idpaket`, `nama`, `program`, `brgkt`, `hari`, `biaya`, `desk`, `kuota`, `entri`) VALUES
+(1, 'Umroh Bersama BST Bengkulu 10 Hari', 'Umroh', '2024-03', 10, '45000000', 'Umroh Bersama BST Bengkulu 10 Hari. Hotel bintang 5', 0, '2024-01-18 15:36:02'),
+(2, 'Umroh Bersama BST Bengkulu 10 Hari + Turki 3 Hari', 'Umroh', '2024-01', 13, '62000000', 'Umroh Bersama BST Bengkulu 10 Hari + Turki 3 Hari', 0, '2024-01-23 09:44:32'),
+(3, 'Haji plus', 'Haji Khusus', '2024-07', 12, '68000000', 'Haji Plus', 50, '2024-02-06 16:31:36');
 
 -- --------------------------------------------------------
 
@@ -150,8 +209,8 @@ CREATE TABLE `syarat` (
 --
 
 INSERT INTO `syarat` (`idsyarat`, `idjamaah`, `jenis`, `gambar`) VALUES
-(1, '082183925322', 'bukunikah', 'WIN_20230916_15_27_08_Pro (2).jpg'),
-(2, '082183925322', 'ktp', 'WIN_20230916_15_15_39_Pro.jpg'),
+(1, '082183925322', 'bukunikah', 'buku nikah.jpg'),
+(2, '082183925322', 'ktp', 'ktp ku.jpg'),
 (3, '082183925322', 'kk', 'WIN_20230916_15_27_09_Pro.jpg'),
 (4, '082183925322', 'aktaijazah', 'WIN_20230916_15_27_08_Pro.jpg'),
 (5, '082183925322', 'bpjs', 'WIN_20221212_07_32_30_Pro.jpg'),
@@ -163,6 +222,18 @@ INSERT INTO `syarat` (`idsyarat`, `idjamaah`, `jenis`, `gambar`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `admuser`
+--
+ALTER TABLE `admuser`
+  ADD PRIMARY KEY (`idadmus`);
+
+--
+-- Indeks untuk tabel `akun`
+--
+ALTER TABLE `akun`
+  ADD PRIMARY KEY (`idakun`);
 
 --
 -- Indeks untuk tabel `bayar`
@@ -199,16 +270,28 @@ ALTER TABLE `syarat`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `admuser`
+--
+ALTER TABLE `admuser`
+  MODIFY `idadmus` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `akun`
+--
+ALTER TABLE `akun`
+  MODIFY `idakun` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT untuk tabel `bayar`
 --
 ALTER TABLE `bayar`
-  MODIFY `idbayar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idbayar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `jamaah`
 --
 ALTER TABLE `jamaah`
-  MODIFY `idjamaah` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `idjamaah` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT untuk tabel `jampaket`
@@ -220,13 +303,13 @@ ALTER TABLE `jampaket`
 -- AUTO_INCREMENT untuk tabel `paket`
 --
 ALTER TABLE `paket`
-  MODIFY `idpaket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idpaket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `syarat`
 --
 ALTER TABLE `syarat`
-  MODIFY `idsyarat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `idsyarat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
